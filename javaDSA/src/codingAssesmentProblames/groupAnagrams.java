889e4df5ca2e6921a09ea495962cc12c3b957a6a
Raghav Pai <raghavpai38@gmail.com>
2026-04-27 19:18:02 +0530

    group Anagram problem

--- javaDSA/src/codingAssesmentProblames/groupAnagrams.java
+++ javaDSA/src/codingAssesmentProblames/groupAnagrams.java
@@ -1,12 +1,34 @@
 package codingAssesmentProblames;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class groupAnagrams {
-	
+	public static List<List<String>> anagramGroup(String []strs){
+		Map <String,List<String>> map=new HashMap<>();
+		
+		for(String word:strs) {
+			char[]ch=word.toCharArray();
+			Arrays.sort(ch);
+			
+			String key=new String(ch);
+			
+			if(!map.containsKey(key)) {
+				map.put(key,new ArrayList<>());
+			}
+			map.get(key).add(word);
+		}
+		return new ArrayList<>(map.values());
+	}
 	
 	
 public static void main(String args[]) {
 	
-	
+	String []strs= {"eat","tea","tan","ate","nat","bat"};
+	System.out.println(anagramGroup(strs));
 }
 }
  
