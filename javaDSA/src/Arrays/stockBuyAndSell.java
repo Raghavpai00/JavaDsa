a4063753688a5ee037a0ad1132908368a9eaa78b
Raghav Pai <raghavpai38@gmail.com>
2026-05-13 20:00:26 +0530

    solved Stock Problem

--- javaDSA/src/Arrays/stockBuyAndSell.java
+++ javaDSA/src/Arrays/stockBuyAndSell.java
@@ -1,5 +1,26 @@
 package Arrays;
 
 public class stockBuyAndSell {
-
+public static int maximumProfit(int prices[]) {
+	int min_Price=prices[0];
+	int max_Profit=0;
+	
+	for(int i=0;i<prices.length;i++) {
+		 if(min_Price>prices[i]) {
+			 min_Price=prices[i]; 
+		 }
+		 int profit=prices[i]-min_Price;
+		 
+		 if(profit>max_Profit) {
+			 max_Profit=profit;
+		 }
+	}
+	return max_Profit;
+	
+}
+	public static void main(String args[]) {
+	int prices[]= {7, 1, 5, 3, 6, 4};
+	
+	System.out.println(maximumProfit(prices));
+}
 }
