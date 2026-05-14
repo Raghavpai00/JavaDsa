4a966d6f6a6a22f7df10646f68f74f1f7d0931b8
Raghav Pai <raghavpai38@gmail.com>
2026-05-14 14:49:09 +0530

    solved the rearrange array with sign problem

--- javaDSA/src/Arrays/rearrangeArrayWithSign.java
+++ javaDSA/src/Arrays/rearrangeArrayWithSign.java
@@ -1,5 +1,33 @@
 package Arrays;
 
 public class rearrangeArrayWithSign {
-
+	public static int[] rearrange(int arr[]) {
+		int n=arr.length;
+		int ans[]=new int[n];
+		
+		int posIndex=0;
+		int negIndex=1;
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>0) {
+				ans[posIndex]=arr[i];
+				posIndex+=2;
+			}else {
+				ans[negIndex]=arr[i];
+				negIndex+=2;
+			}
+		}
+		return ans;
+	}
+	
+	
+public static void main(String args[]) {
+	int arr[] = {1, 2, -4, -5};
+	int []result=rearrange(arr);
+	
+	for(int nums:result) {
+		System.out.print(nums+"  ");
+	}
+	
+}
 }
