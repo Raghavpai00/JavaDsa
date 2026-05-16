8329aef3cb1c030645559cae20a69e9b6b6f8da5
Raghav Pai <raghavpai38@gmail.com>
2026-05-16 09:31:54 +0530

    kadanes algorithm bug fix

--- javaDSA/src/Arrays/KadanesAlgorithm.java
+++ javaDSA/src/Arrays/KadanesAlgorithm.java
@@ -5,7 +5,7 @@ public static int maxSubArray(int arr[]) {
 	int maxSum=arr[0];
 	int currentSum=arr[0];
 	
-	for(int i=0;i<arr.length;i++) {
+	for(int i=1;i<arr.length;i++) {
 	currentSum=Math.max(arr[i],currentSum+arr[i]);
 	maxSum=Math.max(maxSum, currentSum);
 	}
